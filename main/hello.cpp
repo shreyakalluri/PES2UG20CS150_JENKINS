a5b9c1b26e8edcb5c04a4c52d18517dadf0d1bf8
shreyakalluri <shreyakalluri@gmail.com>
2023-02-14 21:20:02 +0530

    idon't like this lab

--- main/hello.cpp
+++ main/hello.cpp
@@ -3,8 +3,8 @@
 using namespace std;
 
 int main(void) {
-  cout << "Hello, World\n";
-  cout << "Hello, Jenkins\n";
-  cout << "I have successfully built and run\n";
+  cout << "Hi\n";
+  cout << "Hehe\n";
+  cout << "I didn't like this lab\n";
   return 0;
 }
